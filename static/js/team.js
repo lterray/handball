a8c0e743ab2373b4f862383c89ef00cab2d3bccc
Gábor Hajdu <gabor.hajdu@codecool.com>
2020-06-11 17:30:53 +0200

    finish voting

--- static/js/team.js
+++ static/js/team.js
@@ -1,5 +1,5 @@
 function init() {
-    let votingButtons =  document.querySelectorAll('.voting-button');
+    let votingButtons = document.querySelectorAll('.voting-button');
     for (let votingButton of votingButtons) {
         votingButton.addEventListener('click', voteClick);
     }
@@ -9,8 +9,11 @@ function voteClick(event) {
     let voteButton = event.target;
     let voteDirection = voteButton.dataset.vote;
 
-    let voteCell = voteButton.closest('.player').querySelector('.vote-cell');
-    voteCell.innerText = parseInt(voteCell.innerText) + (voteDirection === 'up' ? 1 : -1);
+    fetch(`/player/1/vote/${voteDirection}`, {method: 'POST'})
+        .then(() => {
+            let voteCell = voteButton.closest('.player').querySelector('.vote-cell');
+            voteCell.innerText = parseInt(voteCell.innerText) + (voteDirection === 'up' ? 1 : -1);
+        });
 }
 
 init();
